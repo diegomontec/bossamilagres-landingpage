78a7ce1b97b691cd9d6d823b20cd6e91f6691591
Yellow Kite <falecom@yellowkite.com.br>
2025-05-14 15:26:11 -0300

    lp-secaoQuinze

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,31 @@
-import { useLayoutEffect } from 'react';
-import gsap from 'gsap';
-import { ScrollTrigger } from 'gsap/ScrollTrigger';
-import { ScrollSmoother } from 'gsap/ScrollSmoother';
+import { useLayoutEffect } from "react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { ScrollSmoother } from "gsap/ScrollSmoother";
 
 gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 
-import Hero from './components/secoes/Hero'
-import SecaoDois from './components/secoes/SecaoDois'
-import SecaoTres from './components/secoes/SecaoTres'
-import SecaoQuatro from './components/secoes/SecaoQuatro'
-import SecaoCinco from './components/secoes/SecaoCinco'
-import SecaoSeis from './components/secoes/SecaoSeis'
-import SecaoSete from './components/secoes/SecaoSete'
-import SecaoOito from './components/secoes/SecaoOito'
-import SecaoNove from './components/secoes/SecaoNove'
-import SecaoDez from './components/secoes/SecaoDez'
-import SecaoDoze from './components/secoes/SecaoDoze';
-import SecaoOnze from './components/secoes/SecaoOnze';
-import SecaoTreze from './components/secoes/secaoTreze';
-import SecaoQuatorze from './components/secoes/SecaoQuatorze';
-import SecaoQuinze from './components/secoes/secaoQuinze';
+import Hero from "./components/secoes/Hero";
+import SecaoDois from "./components/secoes/SecaoDois";
+import SecaoTres from "./components/secoes/SecaoTres";
+import SecaoQuatro from "./components/secoes/SecaoQuatro";
+import SecaoCinco from "./components/secoes/SecaoCinco";
+import SecaoSeis from "./components/secoes/SecaoSeis";
+import SecaoSete from "./components/secoes/SecaoSete";
+import SecaoOito from "./components/secoes/SecaoOito";
+import SecaoNove from "./components/secoes/SecaoNove";
+import SecaoDez from "./components/secoes/SecaoDez";
+import SecaoDoze from "./components/secoes/SecaoDoze";
+import SecaoOnze from "./components/secoes/SecaoOnze";
+import SecaoTreze from "./components/secoes/SecaoTreze";
+import SecaoQuatorze from "./components/secoes/SecaoQuatorze";
+import SecaoQuinze from "./components/secoes/SecaoQuinze";
 
 function App() {
   useLayoutEffect(() => {
     const smoother = ScrollSmoother.create({
-      wrapper: '#smooth-wrapper',
-      content: '#smooth-content',
+      wrapper: "#smooth-wrapper",
+      content: "#smooth-content",
       smooth: 1.5,
       effects: true,
     });
@@ -55,8 +55,7 @@ function App() {
         <SecaoQuinze />
       </div>
     </div>
-  )
+  );
 }
 
-export default App
-
+export default App;
